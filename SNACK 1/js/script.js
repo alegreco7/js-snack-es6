bb00fe5a2b46b190da23d4898bee86b600b8c8b9
alegreco7 <alexgreco2002@gmail.com>
2023-12-06 17:28:17 +0100

    snack1 bikes object

---
+++ SNACK 1/js/script.js
@@ -0,0 +1,35 @@
+//SNACK 1
+const bikes = [
+    {
+        name: "Rose X-Lite 04",
+        peso: "7",
+    },
+    {
+        name: "TCR Advanced",
+        peso: "5"
+    },
+    {
+        name: "KTM Revelator",
+        peso: "10"
+    },
+    {
+        name: "Endurace CF SLX 8",
+        peso: "7"
+    },
+    {
+        name: "Raise AXS 07",
+        peso: "8"
+    },
+    {
+        name: "Aero M20ILTD",
+        peso: "5"
+    },
+    {
+        name: "Scott Addict SE",
+        peso: "9"
+    },
+    {
+        name: "Litening Air C:68X",
+        peso: "6"
+    },
+];
